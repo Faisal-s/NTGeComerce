7a3f8ced85dc008b89332e87e5af8384b5af822f
fsami <fsami@ntgclarity.com>
2022-11-20 11:17:45 +0300

    files

---
+++ src/main/java/NTGECOM/example/NTGECOM/NtgecomApplication.java
@@ -0,0 +1,13 @@
+package NTGECOM.example.NTGECOM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtgecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtgecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NTGECOM/example/NTGECOM/NtgecomApplicationTests.java
@@ -0,0 +1,13 @@
+package NTGECOM.example.NTGECOM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtgecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
